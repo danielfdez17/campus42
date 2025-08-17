b9bfb4a2d2aead81b9b3b4d28d80bc9a073e1488
danielfdez17 <daniel17dev@gmail.com>
2025-08-17 16:25:32 +0200

    feat: ft_strcat.c done

---
+++ cursus/libft/srcs/ft_strcat.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strcat(char *restrict dst, const char *restrict src)
+{
+	char	*result;
+
+	result = malloc(sizeof(char) * (ft_strlen(dst) + ft_strlen(src) + 1));
+	if (!result)
+		return (NULL);
+	while (*dst)
+		*result++ = *dst++;	
+	while (*src)
+		*result++ = *src++;
+	*result = '\0';
+	return (result);
+}
