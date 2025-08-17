91a5305b8a036dd60db5d05c37b0f92b312cfd92
danielfdez17 <daniel17dev@gmail.com>
2025-08-17 16:40:15 +0200

    feat: ft_strlcat.c done

---
+++ cursus/libft/srcs/ft_strlcat.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "libft.h"
+
+int	ft_min(int a, int b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+size_t	ft_strlcat(char *dst, const char *restrict src, size_t dsize)
+{
+	size_t	total_size;
+
+	total_size = ft_strlen(dst) + ft_min(ft_strlen(src), (int)dsize) + 1;
+	while (*dst)
+		*dst++;
+	while (*src && dsize--)
+		*dst++ = *src++;
+	*dst = '\0';
+	return (total_size);
+}
