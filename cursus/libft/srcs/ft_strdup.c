2142b6269dc1def8b1ccd1d3b05e5c71dcce204e
danielfdez17 <daniel17dev@gmail.com>
2025-08-17 16:12:50 +0200

    feat: ft_strdup done

---
+++ cursus/libft/srcs/ft_strdup.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	size_t	i;
+	char	*dup;
+
+	i = 0;
+	dup = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!dup)
+		return (NULL);
+	while (s[i])
+	{
+		dup[i] = s[i];
+		++i;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
