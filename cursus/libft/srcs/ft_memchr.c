ae5f12a23b6be953e3650a92ac0f83859cffc12e
danielfdez17 <daniel17dev@gmail.com>
2025-08-17 14:56:09 +0200

    feat: ft_memchr done

---
+++ cursus/libft/srcs/ft_memchr.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (ptr[i] == (unsigned char)c)
+			return (ptr[i]);
+		++i;
+	}
+	return (NULL);
+}
