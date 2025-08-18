7fb75bbace3c71e4a71c5fa2ae8fb20368af03a6
danielfdez17 <daniel17dev@gmail.com>
2025-08-18 17:38:21 +0200

    feat: ft_isalpha done

--- cursus/libft/srcs/ft_isalpha.c
+++ cursus/libft/srcs/ft_isalpha.c
@@ -1,8 +1,7 @@
 
-int	ft_isalpha(int c)
+int	ft_isdigit(int c)
 {
-	if (((unsigned char)c >= 'a' && (unsigned char)c <= 'z')\
-		|| ((unsigned char)c >= 'A' && (unsigned char)c <= 'Z'))
+	if ((unsigned char)c >= '0' && (unsigned char)c <= '9')
 		return (1);
 	return (0);
 }
