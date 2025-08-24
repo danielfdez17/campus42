0b47ea34b67ded4f9e3d8b539acc301de770e5e5
danielfdez17 <daniel17dev@gmail.com>
2025-08-24 17:53:12 +0200

    feat: ft_strmapi done

---
+++ cursus/libft/srcs/ft_strmapi.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*result;
+
+	result = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		result[i] = f(i, s[i]);
+		++i;
+	}
+	result[i] = '\0';
+	return (result);
+}
