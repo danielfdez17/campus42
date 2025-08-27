2b3cdd1acec04eb794d23bb6896484cb911ea007
danielfdez17 <daniel17dev@gmail.com>
2025-08-27 11:00:24 +0200

    feat: added some function definitions and t_list structure

--- cursus/libft/includes/libft.h
+++ cursus/libft/includes/libft.h
@@ -2,6 +2,13 @@
 # define LIBFT_H
 #include <stdlib.h>
 
+typedef struct	s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}	t_list;
+
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
@@ -14,5 +21,7 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 void	ft_putchar(char c);
 void	ft_putstr(char const *c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char const *s, int fd);
 
 # endif
