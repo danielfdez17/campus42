f1b73f1c2a97b94d8eceddd518c5ed571a7f7d00
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 09:28:25 +0200

    feat: added testing main

--- cursus/libft/srcs/ft_isascii.c
+++ cursus/libft/srcs/ft_isascii.c
@@ -5,3 +5,16 @@ int	ft_isascii(int c)
 		return (1);
 	return (0);
 }
+
+// #include <ctype.h>
+// #include <stdio.h>
+// int main(int ac, char **av)
+// {
+// 	int i = 1;
+// 	while (ac-- > 1)
+// 	{
+// 		printf("char: %c\nisascii: %d\nft_isascii: %d\n---\n", av[i][0], isascii(av[i][0]), ft_isascii(av[i][0]));
+// 		++i;
+// 	}
+// 	return (0);
+// }
