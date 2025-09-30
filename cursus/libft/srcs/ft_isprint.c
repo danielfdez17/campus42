5c551a80fe56036adda0412aafb92052a676b150
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 09:32:52 +0200

    feat: added testing main

--- cursus/libft/srcs/ft_isprint.c
+++ cursus/libft/srcs/ft_isprint.c
@@ -5,3 +5,16 @@ int	ft_isprint(int c)
 		return (1);
 	return (0);
 }
+
+// #include <ctype.h>
+// #include <stdio.h>
+// int main(int ac, char **av)
+// {
+// 	int i = 1;
+// 	while (ac-- > 1)
+// 	{
+// 		printf("char: %c\nisprint: %d\nft_isprint: %d\n---\n", av[i][0], isprint(av[i][0]), ft_isprint(av[i][0]));
+// 		++i;
+// 	}
+// 	return (0);
+// }
