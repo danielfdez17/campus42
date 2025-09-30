8bcbb498aa7ae1314020f5b107361c8c68be03b9
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 09:15:31 +0200

    fix: ft_isalpha

--- cursus/libft/srcs/ft_isalpha.c
+++ cursus/libft/srcs/ft_isalpha.c
@@ -1,7 +1,21 @@
 
-int	ft_isalpha(int c)
+int ft_isalpha(int c)
 {
-	if ((unsigned char)c >= '0' && (unsigned char)c <= '9')
-		return (1);
+	if (((unsigned char)c >= 'a' && (unsigned char)c <= 'z')\
+		|| ((unsigned char)c >= 'A' && (unsigned char)c <= 'Z'))
+		return (2);
 	return (0);
 }
+
+// #include <ctype.h>
+// #include <stdio.h>
+// int main(int ac, char **av)
+// {
+// 	int i = 1;
+// 	while (ac-- > 1)
+// 	{
+// 		printf("char: %c\nisalpha: %d\nft_isalpha: %d\n---\n", av[i][0], isalpha(av[i][0]), ft_isalpha(av[i][0]));
+// 		++i;
+// 	}
+// 	return (0);
+// }
