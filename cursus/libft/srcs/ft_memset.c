427ef19716a6e9fcd228b2415fcca88d6ba87f6d
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 09:49:46 +0200

    feat: added testing main but could not test

--- cursus/libft/srcs/ft_memset.c
+++ cursus/libft/srcs/ft_memset.c
@@ -9,3 +9,17 @@ void	*ft_memset(void *s, int c, size_t n)
 		ptr[n] = (unsigned char)c;
 	return (s);
 }
+
+// #include <string.h>
+// #include <stdio.h>
+// int main(int ac, char **av)
+// {
+// 	(void)ac;
+// 	char *str = "Hello World!";
+// 	printf("str: %s\n", str);
+// 	printf("char: %c\n", av[3][0]);
+// 	printf("bytes: %d\n", atoi(av[4]));
+// 	printf("memset: %s\n", (char *)memset(str, atoi(av[3]), atoi(av[4])));
+// 	printf("ft_memset: %s\n", (char *)ft_memset(str, atoi(av[3]), atoi(av[4])));
+// 	return (0);
+// }
