e52e4919aaf1b8072e823c870e53a5d4ab078ff3
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 09:17:05 +0200

    fix: ft_isdigit

--- cursus/libft/srcs/ft_isdigit.c
+++ cursus/libft/srcs/ft_isdigit.c
@@ -1,8 +1,20 @@
 
 int	ft_isdigit(int c)
 {
-	if (((unsigned char)c >= 'a' && (unsigned char)c <= 'z')\
-		|| ((unsigned char)c >= 'A' && (unsigned char)c <= 'Z'))
+	if ((unsigned char)c >= '0' && (unsigned char)c <= '9')
 		return (1);
 	return (0);
 }
+
+// #include <ctype.h>
+// #include <stdio.h>
+// int main(int ac, char **av)
+// {
+// 	int i = 1;
+// 	while (ac-- > 1)
+// 	{
+// 		printf("char: %c\nisdigit: %d\nft_isdigit: %d\n---\n", av[i][0], isdigit(av[i][0]), ft_isdigit(av[i][0]));
+// 		++i;
+// 	}
+// 	return (0);
+// }
