e5fec043b0df17bdd26a771064770f58f3409baa
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 11:41:25 +0200

    feat: added testing main

--- cursus/libft/srcs/ft_strchr.c
+++ cursus/libft/srcs/ft_strchr.c
@@ -7,6 +7,18 @@ char	*ft_strchr(const char *s, int c)
 	ptr = (char *)s;
 	while (*ptr)
 		if (*ptr++ == (unsigned char)c)
-			return (ptr);
+			return (--ptr);
 	return (NULL);
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// int main(void)
+// {
+// 	const char *s = "asdfasdfa";
+// 	int c = 'o';
+// 	printf("ft_strchr: %s\n", ft_strchr(s, c));
+// 	printf("strchr: %s\n---\n", strchr(s, c));
+// 	return (0);
+// }
