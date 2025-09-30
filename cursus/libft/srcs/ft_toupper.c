af75301d5c65bddf577d132e499854ebb586a100
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 11:32:08 +0200

    feat: added testing main

--- cursus/libft/srcs/ft_toupper.c
+++ cursus/libft/srcs/ft_toupper.c
@@ -2,6 +2,22 @@
 int	ft_toupper(int c)
 {
 	if ((unsigned char)c >= 'a' && (unsigned char)c <= 'z')
-		return (c + 32);
+		return (c - 32);
 	return (c);
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <ctype.h>
+// int main(int ac, char **av)
+// {
+// 	int i = 0;
+// 	while (ac-- > 1)
+// 	{
+// 		printf("char: %c\n", av[i][0]);
+// 		printf("ft_toupper: %c\n", ft_toupper(av[i][0]));
+// 		printf("toupper: %c\n---\n", toupper(av[i][0]));
+// 		++i;
+// 	}
+// 	return (0);
+// }
