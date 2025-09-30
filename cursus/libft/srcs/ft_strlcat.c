82ed426ab93f5e4077899e3b5c3c7b600af5d24b
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 11:25:54 +0200

    feat: added testing main but could not be tested (segmentation fault)

--- cursus/libft/srcs/ft_strlcat.c
+++ cursus/libft/srcs/ft_strlcat.c
@@ -1,5 +1,14 @@
 #include <stdlib.h>
-#include "../includes/libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		++i;
+	return (i);
+}
 
 int	ft_min(int a, int b)
 {
@@ -20,3 +29,16 @@ size_t	ft_strlcat(char *dst, const char *restrict src, size_t dsize)
 	*dst = '\0';
 	return (total_size);
 }
+
+// #include <stdio.h>
+// #include <string.h>
+// int main(void)
+// {
+// 	const char *src = "Hola buenos dias";
+// 	char *dest = "destinooooooooo";
+// 	printf("src: %s\n", src);
+// 	printf("dest: %s\n", dest);
+// 	printf("ft_strlcat: %ld\n", ft_strlcat(dest, src, 3));
+// 	printf("strlcat: %ld\n", strlcat(dest, src, 3));
+// 	return (0);
+// }
