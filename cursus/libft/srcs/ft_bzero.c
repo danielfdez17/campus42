495f2f87044c1c243cd3f39630630f2fd81995b9
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 10:13:19 +0200

    feat: added testing main but could not test

--- cursus/libft/srcs/ft_bzero.c
+++ cursus/libft/srcs/ft_bzero.c
@@ -8,3 +8,17 @@ void	ft_bzero(void *s, size_t n)
 	while (n--)
 		ptr[n] = 0;
 }
+
+// #include <strings.h>
+// #include <stdio.h>
+// int main(int ac, char **av)
+// {
+// 	(void)ac;
+// 	char *str = "Hello World!";
+// 	printf("str: %s\n", str);
+// 	printf("bytes: %d\n", atoi(av[2]));
+// 	// printf("bzero: %s\n", bzero(str, atoi(av[4])));
+// 	ft_bzero(str, atoi(av[2]));
+// 	printf("ft_bzero: %s\n", str);
+// 	return (0);
+// }
