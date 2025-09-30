0556b71adfd5f732534685837b4f6c5c230f256d
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 09:37:48 +0200

    feat: added testing main

--- cursus/libft/srcs/ft_strlen.c
+++ cursus/libft/srcs/ft_strlen.c
@@ -9,3 +9,16 @@ size_t	ft_strlen(const char *s)
 		++i;
 	return (i);
 }
+
+// #include <string.h>
+// #include <stdio.h>
+// int main(int ac, char **av)
+// {
+// 	int i = 1;
+// 	while (ac-- > 1)
+// 	{
+// 		printf("str: %s\nstrlen: %lld\nft_strlen: %lld\n---\n", av[i], strlen(av[i]), ft_strlen(av[i]));
+// 		++i;
+// 	}
+// 	return (0);
+// }
