477ee3ab71941ec403a544982697d35082ba8f9c
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 11:43:25 +0200

    feat: added testing main

--- cursus/libft/srcs/ft_strrchr.c
+++ cursus/libft/srcs/ft_strrchr.c
@@ -1,5 +1,14 @@
 #include <stdlib.h>
-#include "../includes/libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		++i;
+	return (i);
+}
 
 char	*ft_strrchr(const char *s, int c)
 {
@@ -11,3 +20,15 @@ char	*ft_strrchr(const char *s, int c)
 			return ((char *)(s + size));
 	return (NULL);
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// int main(void)
+// {
+// 	const char *s = "oasdfasd";
+// 	int c = 'o';
+// 	printf("ft_strrchr: %s\n", ft_strrchr(s, c));
+// 	printf("strrchr: %s\n---\n", strrchr(s, c));
+// 	return (0);
+// }
