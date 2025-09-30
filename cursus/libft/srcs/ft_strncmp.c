e2d97069b8fd388f8dd76ebb52930c0b77058fd4
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 11:45:18 +0200

    feat: added testing main

--- cursus/libft/srcs/ft_strncmp.c
+++ cursus/libft/srcs/ft_strncmp.c
@@ -13,3 +13,16 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return (0);
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// int main(void)
+// {
+// 	const char *s1 = "owieuralksdjf";
+// 	const char *s2 = "ualksdjf";
+// 	size_t c = 7;
+// 	printf("ft_strncmp: %d\n", ft_strncmp(s1, s2, c));
+// 	printf("strncmp: %d\n---\n", strncmp(s1, s2, c));
+// 	return (0);
+// }
