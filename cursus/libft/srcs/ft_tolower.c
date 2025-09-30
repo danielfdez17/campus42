1475a55ab879b525af0122774bc88753caf53d12
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 11:34:38 +0200

    feat: added testing main

--- cursus/libft/srcs/ft_tolower.c
+++ cursus/libft/srcs/ft_tolower.c
@@ -2,6 +2,22 @@
 int	ft_tolower(int c)
 {
 	if ((unsigned char)c >= 'A' && (unsigned char)c <= 'Z')
-		return (c - 32);
+		return (c + 32);
 	return (c);
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <ctype.h>
+// int main(int ac, char **av)
+// {
+// 	int i = 1;
+// 	while (--ac)
+// 	{
+// 		printf("char: %c\n", av[i][0]);
+// 		printf("ft_tolower: %c\n", ft_tolower(av[i][0]));
+// 		printf("tolower: %c\n---\n", tolower(av[i][0]));
+// 		++i;
+// 	}
+// 	return (0);
+// }
