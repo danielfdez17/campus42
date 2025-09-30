a8144e01c61ed1a5db6ad0814768bdfe595c6413
danielfdez17 <daniel17dev@gmail.com>
2025-09-30 12:07:39 +0200

    feat: added ft_calloc without testing it

---
+++ cursus/libft/srcs/ft_calloc.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
+	while (n--)
+		ptr[n] = (unsigned char)c;
+	return (s);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (malloc(0));
+	ptr = mallock(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, 0, nmemb * size);
+	return (ptr);
+}
