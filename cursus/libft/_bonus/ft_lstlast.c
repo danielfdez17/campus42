aa3e3d19e3b577be8b4fbe07955a2eccdf58e36c
danielfdez17 <daniel17dev@gmail.com>
2025-10-01 17:55:19 +0200

    feat: ft_lstlast

--- cursus/libft/_bonus/ft_lstlast.c
+++ cursus/libft/_bonus/ft_lstlast.c
@@ -2,5 +2,15 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	
+	t_list	*ptr;
+	t_list	*prev;
+
+	ptr = lst;
+	prev = NULL;
+	while (ptr != NULL)
+	{
+		prev = ptr;
+		ptr = ptr->next;
+	}
+	return (prev);
 }
