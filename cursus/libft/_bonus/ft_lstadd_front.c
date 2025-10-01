ee9202988c53bd0739ed166d61acf89d9f92ce3a
danielfdez17 <daniel17dev@gmail.com>
2025-10-01 17:48:56 +0200

    feat: ft_lstadd_front

--- cursus/libft/_bonus/ft_lstadd_front.c
+++ cursus/libft/_bonus/ft_lstadd_front.c
@@ -1,6 +1,13 @@
 #include "libft.h"
 
+typedef struct	s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	
+	new->next = *lst;
+	*lst = new;
 }
