2b1af58c3c1b4e97ac16580fd50736aede7ca259
danielfdez17 <daniel17dev@gmail.com>
2025-10-01 08:42:10 +0200

    feat: ft_putendl_fd static putchar_fd && putstr_fd

--- cursus/libft/srcs/ft_putendl_fd.c
+++ cursus/libft/srcs/ft_putendl_fd.c
@@ -1,5 +1,21 @@
-#include "../includes/libft.h"
+#include <unistd.h>
 
+static void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+
+static void	ft_putstr_fd(char const *s, int fd)
+{
+	if (!s)
+		return ;
+	while (*s)
+	{
+		ft_putchar_fd(*s, fd);
+		s++;
+	}
+}
 void	ft_putendl_fd(char const *s, int fd)
 {
 	if (!s)
